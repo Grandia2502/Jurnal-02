033d02162bf78a170c0d122d1a9d5da0ebfe7063
Grandia M <grandsteve.gm@gmail.com>
2021-09-29 23:59:16 +0700

    ain

---
+++ Tugas-Jurnal-01/Tugas.java
@@ -0,0 +1,8 @@
+public class Tugas {
+
+	public static void main(String[] args) {
+		System.out.println("Nama  : Grandia Muhammad");
+		System.out.println("NIM   : 6706213096");
+		System.out.println("Kelas : D3RPLA-45-01");
+	}
+}
